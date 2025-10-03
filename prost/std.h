75596744688a315cfaba7273a3e09100f5bca7b8
dev <jmoritz38@gmail.com>
2025-10-03 16:53:48 +0200

    added more std functions

--- prost/std.h
+++ prost/std.h
@@ -22,8 +22,71 @@ void print(ProstVM *vm) {
 
 }
 
+void add(ProstVM *vm) {
+    Word w1 = p_pop(vm);
+    Word w2 = p_pop(vm);
+    uint64_t result = 0;
+
+    if (w1.type == WINT) result += w1.as_int;
+    if (w2.type == WINT) result += w2.as_int;
+    if (w1.type == WUINT64) result += w1.as_uint64;
+    if (w2.type == WUINT64) result += w2.as_uint64;
+
+    p_push(vm, word_uint64(result));
+}
+
+void sub(ProstVM *vm) {
+    Word w1 = p_pop(vm);
+    Word w2 = p_pop(vm);
+    int64_t result = 0;
+
+    if (w1.type == WINT) result = w1.as_int;
+    else if (w1.type == WUINT64) result = w1.as_uint64;
+
+    if (w2.type == WINT) result -= w2.as_int;
+    else if (w2.type == WUINT64) result -= w2.as_uint64;
+
+    p_push(vm, word_uint64(result));
+}
+
+void mul(ProstVM *vm) {
+    Word w1 = p_pop(vm);
+    Word w2 = p_pop(vm);
+    uint64_t result = 1;
+
+    if (w1.type == WINT) result = w1.as_int;
+    else if (w1.type == WUINT64) result = w1.as_uint64;
+
+    if (w2.type == WINT) result *= w2.as_int;
+    else if (w2.type == WUINT64) result *= w2.as_uint64;
+
+    p_push(vm, word_uint64(result));
+}
+
+void divi(ProstVM *vm) {
+    Word w1 = p_pop(vm);
+    Word w2 = p_pop(vm);
+    if ((w2.type == WINT && w2.as_int == 0) || (w2.type == WUINT64 && w2.as_uint64 == 0)) {
+        fprintf(stderr, "ERROR: Division by zero\n");
+        vm->status = P_ERR_GENERAL_VM_ERROR;
+        vm->running = false;
+        p_push(vm, word_uint64(0));
+        return;
+    }
+
+    uint64_t numerator = (w1.type == WINT) ? w1.as_int : w1.as_uint64;
+    uint64_t denominator = (w2.type == WINT) ? w2.as_int : w2.as_uint64;
+
+    p_push(vm, word_uint64(numerator / denominator));
+}
+
+
 void register_std(ProstVM *vm) {
     p_register_external(vm, "print", print);
+    p_register_external(vm, "add", add);
+    p_register_external(vm, "sub", sub);
+    p_register_external(vm, "mul", mul);
+    p_register_external(vm, "divi", divi);
 }
 
 
